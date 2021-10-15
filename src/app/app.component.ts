18543eb4792769a686380ff6b62e399ad54587ae
MusatovNick <mysatovnick@gmail.com>
2021-10-15 15:38:20 +0300

    Setting start project
    * Implemented eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  title = 'ulduar';
+export class AppComponent {
 
-  ngOnInit() {
-    console.log('as asd1f asd as1ddf a1111sdf asa1d' );
-
-    const a = { b: 3 };
-
-    console.log(a, 'asd11fa 111 sd 11111111' );
-    console.log(a, 'asd11fa 111 sd 11111111' );
-  }
-
-  getA() {
-    return 10;
-  }
 }
