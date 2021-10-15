44d513fc1662d8aef54d4ac2d0be3cb5ebcb8126
MusatovNick <mysatovnick@gmail.com>
2021-10-15 15:17:37 +0300

    Setting start project
    * Implemented eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,11 @@ export class AppComponent implements OnInit {
   title = 'ulduar';
 
   ngOnInit() {
-    console.log('as asdf asd asddf a111sdf asasd' );
+    console.log('as asdf asd as1ddf a111sdf asasd' );
 
     const a = { b: 3 };
 
-    console.log(a, 'asdfa 111 sd 1111111' );
+    console.log(a, 'asd1fa 111 sd 1111111' );
   }
 
   getA() {
