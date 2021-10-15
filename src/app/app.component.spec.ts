f537d0b16d4f661b15ec464f474caed3ee9b89f8
MusatovNick <mysatovnick@gmail.com>
2021-10-15 14:51:49 +0300

    Setting start project
    * Implemented eslint

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -6,10 +6,10 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
       ],
     }).compileComponents();
   });
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ulduar'`, () => {
+  it('should have as title \'ulduar\'', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ulduar');
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ulduar'
+export class AppComponent implements OnInit {
+  title = 'ulduar';
 
   ngOnInit() {
-    console.log('asdfasd')
+    console.log('asdfasd');
+
+    const a = { b: 3 };
+
+    console.log(a);
+  }
+
+  getA() {
+    return 10;
   }
 }
