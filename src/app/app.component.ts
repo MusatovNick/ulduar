10e5bc73ad1e8bb29260fdd72d56b9740826b26f
MusatovNick <mysatovnick@gmail.com>
2021-10-15 13:50:23 +0300

    Setting start project
    * Implemented eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ulduar';
+  title = 'ulduar'
+
+  ngOnInit() {
+    console.log('asdfasd')
+  }
 }
